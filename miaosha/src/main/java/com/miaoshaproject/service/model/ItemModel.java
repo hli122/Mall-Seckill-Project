c2a5ae168c7a468df614cd11d076ef893b12cc89
hli122 <haorangowest@gmail.com>
2021-08-08 11:08:15 -0700

    design domain model and then the database of product, use mybatis generator to generate the corresponding objects

---
+++ miaosha/src/main/java/com/miaoshaproject/service/model/ItemModel.java
@@ -0,0 +1,81 @@
+package com.miaoshaproject.service.model;
+
+import java.math.BigDecimal;
+
+public class ItemModel {
+    private Integer id;
+
+    // item name
+    private String title;
+
+    // item price
+    private BigDecimal price;
+
+    // item storage
+    private Integer stock;
+
+    // item description
+    private String description;
+
+    // item sales
+    private Integer sales;
+
+    // item description picture's url
+    private String imgUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getSales() {
+        return sales;
+    }
+
+    public void setSales(Integer sales) {
+        this.sales = sales;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
