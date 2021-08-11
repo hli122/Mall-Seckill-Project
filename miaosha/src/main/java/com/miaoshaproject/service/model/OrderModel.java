84d0416737f0be98602918aa6f0d3a4cc9f9d118
hli122 <haorangowest@gmail.com>
2021-08-10 21:33:29 -0700

    Order Model created

---
+++ miaosha/src/main/java/com/miaoshaproject/service/model/OrderModel.java
@@ -0,0 +1,71 @@
+package com.miaoshaproject.service.model;
+
+import java.math.BigDecimal;
+
+public class OrderModel {
+    // 202108100001282
+    private String id;
+
+    // user id
+    private String userId;
+
+    // item id
+    private String itemId;
+
+    // the price of the item when purchased
+    private BigDecimal itemPrice;
+
+    // purchase number
+    private Integer amount;
+
+    // purchase price
+    private BigDecimal orderPrice;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(BigDecimal orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public BigDecimal getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(BigDecimal itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+}
